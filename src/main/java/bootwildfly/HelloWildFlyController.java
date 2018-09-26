0099dcc6f91be183e07f9271cdfc8465fc18aef7
felipetux <felipe.tux@gmail.com>
2018-09-26 10:28:03 -0300

    Update HelloWildFlyController.java

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -5,10 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWildFlyController {
+    
+    final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
 
     @RequestMapping("hello")
     public String sayHello(){
         return ("Hello, SpringBoot on Wildfly");
+    }
+    
+    
+    @RequestMapping("horaoficial")
+    public String getHoraOficial(){
+        System.out.println(dateFormat.format(now.getTime()))
+        return ("HoraOficial");
     }
 }
